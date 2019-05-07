d8b6e7a34802ed9bf7efc53db6ca16f43ee5f288
John Tendik <jtendik@igloosoftware.com>
2019-05-07 10:12:50 -0400

    pd day yay learning time

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,11 +1,23 @@
 const mutations = {
-  createDog(parent, args, context, info) {
-    // create a dog or whatever
-    global.dogs = global.dogs || [];
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+
+    return item;
   }
+
+  // createDog(parent, args, context, info) {
+  //   // create a dog or whatever
+  //   global.dogs = global.dogs || [];
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
 };
 
 module.exports = mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,9 +1,13 @@
+const { forwardTo } = require('prisma-binding');
+
 const Query = {
 
-  dogs(parent, args, context, info) {
-    global.dogs = global.dogs || [];
-    return global.dogs;
-  }
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
+
+  items: forwardTo('db'),
 
 };
 
